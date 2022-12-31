fe0ec87e6b924d75a7db74bec5f43ef57dfabdd6
svitlanapl <105806771+svitlanapl@users.noreply.github.com>
2022-12-31 23:42:26 +0000

    Deploying to gh-pages from @ svitlanapl/goit-react-hw-05-movies@f60547c8675b9dc109e92fe46c13d502161d90a2 🚀

---
+++ static/js/922.f61a95f8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[922],{8922:function(e,t,a){a.r(t);a(184);t.default=function(){}}}]);
+//# sourceMappingURL=922.f61a95f8.chunk.js.map
